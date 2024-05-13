f97e346d05258878e5e91ac3c508866b0fa249b4
benskia <benski.abraham@gmail.com>
2024-05-13 03:01:19 -0500

    first commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    tn = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(tn)
+    input()
+
+if __name__ == "__main__":
+    main()
