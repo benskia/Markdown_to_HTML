dbb1117a4062e87a66f99eb59fcd5d4535c43dce
benskia <benski.abraham@gmail.com>
2024-05-16 15:53:11 -0500

    work on split_nodes_delimiter ALSO LOTS OF RAIN

--- src/textnode.py
+++ src/textnode.py
@@ -39,9 +39,9 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     if len(old_nodes) == 0:
-        return []
+        raise Exception("Node input was empty.")
 
-    text_type_codes = {
+    delimiter_text_types = {
         "*": "italic",
         "**": "bold",
         "`": "code",
@@ -49,13 +49,24 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     new_nodes = []
     for node in old_nodes:
-        # We're only splitting "text" type TextNodes.
+        # We're only splitting "text_type" TextNodes.
         if node.text_type != text_type:
             new_nodes.append(node)
             continue
         # Is the delimiter valid?
-        text_type_code = text_type_codes.get(delimiter, "invalid")
+        text_type_code = delimiter_text_types.get(delimiter, "invalid")
         if text_type_code == "invalid":
+            new_nodes.append(node)
+            continue
+
+        text_segments = node.text.split(delimiter)
+        num_segments = len(text_segments)
+        # A single segment indicates no split took place.
+        if num_segments == 1:
+            new_nodes.append(node)
+            continue
+        # An even number of segments indicates solo delimiters were encountered.
+        if num_segments % 2 == 0:
             new_nodes.append(node)
             continue
 
