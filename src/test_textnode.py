eb1bc62c31a595f238106750580968cb1ee39a79
benskia <benski.abraham@gmail.com>
2024-05-16 15:05:07 -0500

    work on split_text_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from htmlnode import LeafNode
-from textnode import TextNode, text_node_to_html_node
+from textnode import TextNode, split_nodes_delimiter, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,7 +22,41 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
-    pass
+    def test_eq(self):
+        test_nodes = [
+            TextNode("", "link"),
+            TextNode("normal text **bold text** normal text", None),
+            TextNode("bold text *italic text* bold text", "bold"),
+            TextNode("italic text *invalid markdown text italic text", "italic"),
+        ]
+
+        result1 = [
+            TextNode("", "link"),
+            TextNode("normal text ", None),
+            TextNode("bold text", "bold"),
+            TextNode(" normal text", None),
+            TextNode("bold text *italic text* bold text", "bold"),
+            TextNode("italic text *invalid markdown text italic text", "italic"),
+        ]
+        result2 = [
+            TextNode("", "link"),
+            TextNode("bold text ", "bold"),
+            TextNode("italic text", "italic"),
+            TextNode(" bold text", "bold"),
+            TextNode("bold text *italic text* bold text", "bold"),
+            TextNode("italic text *invalid markdown text italic text", "italic"),
+        ]
+        result3 = [
+            TextNode("", "link"),
+            TextNode("normal text **bold text** normal text", None),
+            TextNode("bold text *italic text* bold text", "bold"),
+            TextNode("italic text *invalid markdown text italic text", "italic"),
+        ]
+
+        input1 = split_nodes_delimiter(test_nodes, "**", None)
+        self.assertEqual(input1, result1)
+        input2 = split_nodes_delimiter(test_nodes, "*", "bold")
+        self.assertEqual(input1, result1)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -38,6 +38,9 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if len(old_nodes) == 0:
+        return []
+
     text_type_codes = {
         "*": "italic",
         "**": "bold",
@@ -46,30 +49,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     new_nodes = []
     for node in old_nodes:
-        # We're only splitting raw text.
-        if node.text_type != "text":
+        # We're only splitting "text" type TextNodes.
+        if node.text_type != text_type:
             new_nodes.append(node)
             continue
-
-        text_segments = node.text.split(delimiter)
-        num_segments = len(text_segments)
-
-        # A size of 1 indicates no split took place.
-        if num_segments == 1:
+        # Is the delimiter valid?
+        text_type_code = text_type_codes.get(delimiter, "invalid")
+        if text_type_code == "invalid":
             new_nodes.append(node)
             continue
-
-        # An even size indicates an odd number of delimiters were encountered - no matching, closing delimiter.
-        if num_segments % 2 == 0:
-            raise Exception(
-                "Encountered an even number of segments after splitting, indicating a delimiter without a matching, closing delimiter."
-            )
-        for i in range(num_segments):
-            if i % 2 == 0:
-                new_nodes.append(TextNode(text_segments[i], text_type))
-            else:
-                new_nodes.append(
-                    TextNode(text_segments[i], text_type_codes.get(delimiter))
-                )
 
     return new_nodes
