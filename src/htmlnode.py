8fb22d33d9a44d367a89e3042a8c2ee47447a4cb
benskia <benski.abraham@gmail.com>
2024-05-17 11:44:16 -0500

    rewrite test_textnode; remove redundant prop conversion in htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,32 +23,26 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
     def __init__(self, value, tag=None, props=None):
-        super().__init__()
-        self.tag = tag
-        self.value = value
-        self.props = props
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value == None:
             raise ValueError("This LeafNode has no value.")
         if self.tag == None:
             return self.value
-        converted_props = "" if self.props == None else f" {self.props_to_html()}"
-        return f"<{self.tag}{converted_props}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
 
 class ParentNode(HTMLNode):
-    def __init__(self, children, tag=None, props=None):
-        super().__init__()
-        self.tag = tag
-        self.children = children
-        self.props = props
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
 
     def to_html(self):
         if self.tag == None:
             raise ValueError("This ParentNode has no tag.")
+        if self.children == None:
+            raise ValueError("This ParentNode has no children.")
         child_node_values = ""
         for child_node in self.children:
             child_node_values += child_node.to_html()
-        converted_props = "" if self.props == None else f" {self.props_to_html()}"
-        return f"<{self.tag}{converted_props}>{child_node_values}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{child_node_values}</{self.tag}>"
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,43 +1,36 @@
 import unittest
 
-from htmlnode import LeafNode
-from textnode import TextNode, split_nodes_delimiter, text_node_to_html_node
+from textnode import TextNode
 
 
 class TestTextNode(unittest.TestCase):
+
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("These nodes are the same.", "bold")
+        node2 = TextNode("These nodes are the same.", "bold")
         self.assertEqual(node, node2)
 
+    def test_eq_false(self):
+        node = TextNode("These nodes have different types.", "bold")
+        node2 = TextNode("These nodes have different types.", "text")
+        self.assertNotEqual(node, node2)
 
-class TestTextNodeToHTMLNode(unittest.TestCase):
-    def test_eq(self):
-        node = TextNode("This is a text node.", "bold")
-        node2 = TextNode("This is a node with an invalid text_type.", "banana")
-        leafnode = LeafNode("This is a text node.", "b")
-        self.assertEqual(print(text_node_to_html_node(node)), print(leafnode))
-        with self.assertRaises(Exception):
-            text_node_to_html_node(node2)
+    def test_eq_false2(self):
+        node = TextNode("These nodes have different text!", "text")
+        node2 = TextNode("These nodes have different text...", "text")
+        self.assertNotEqual(node, node2)
 
+    def test_eq_url(self):
+        node = TextNode("These nodes have the same url.", "code", "https://example.com")
+        node2 = TextNode(
+            "These nodes have the same url.", "code", "https://example.com"
+        )
+        self.assertEqual(node, node2)
 
-class TestSplitNodesDelimiter(unittest.TestCase):
-    def test_eq(self):
-        node1 = TextNode("normal `code` normal", "text")
-        node2 = TextNode("bold *italic* bold", "bold")
-        node3 = TextNode("normal **invalid normal", "text")
-        node4 = TextNode("normal **bold** normal", "text")
-        input = [node1, node2, node3, node4]
-
-        sc1 = TextNode("normal ", "text")
-        sc2 = TextNode("code", "code")
-        sc3 = TextNode(" normal", "text")
-        code_output = [sc1, sc2, sc3, node2, node3]
-
-        self.assertEqual(split_nodes_delimiter(input, "`", "code"), code_output)
-        self.assertEqual(split_nodes_delimiter(input, "*", "italic"), input)
-        with self.assertRaises(Exception):
-            split_nodes_delimiter(input, "**", "bold")
+    def test_repr(self):
+        input = TextNode("This tests repr!", "link", "https://example.com")
+        output = "TextNode(This tests repr!, link, https://example.com)"
+        self.assertEqual(repr(input), output)
 
 
 if __name__ == "__main__":
