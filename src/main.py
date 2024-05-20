09e1772e72280f747bdff0ba77c1e2859cc36907
benskia <benski.abraham@gmail.com>
2024-05-20 15:11:29 -0500

    copy static utils -> main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
+from os import path, listdir, mkdir
+from shutil import copy, rmtree
 from textnode import TextNode
 
 
 def main():
-    tn = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(tn)
+    source_directory = "./static"
+    desination_directory = "./public"
+    build_static_site_project(source_directory, desination_directory)
+
+
+def build_static_site_project(source, destination):
+    rmtree(destination)
+    mkdir(destination)
+    ls = get_ls(source)
+    for item in ls:
+        pass
+
+
+def get_ls(filepath):
+    try:
+        ls = listdir(filepath)
+        print(ls)
+        return ls
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
