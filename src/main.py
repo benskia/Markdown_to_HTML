e9c15d8c81b8fe2c7b9f31a6b8fc8382d2444526
benskia <benski.abraham@gmail.com>
2024-05-20 16:23:41 -0500

    complete copy_static

--- src/main.py
+++ src/main.py
@@ -1,33 +1,40 @@
 from os import listdir, mkdir
-from os.path import exists, join, isfile
+from os.path import exists, join, isfile, isdir
 from shutil import copy, rmtree
 from textnode import TextNode
 
 
 def main():
-    source_directory = "./static"
-    destination_directory = "./public"
-    copy_static(source_directory, destination_directory)
+    source_root = "./static"
+    destination_root = "./public"
+    if exists(destination_root):
+        rmtree(destination_root)
+    copy_static(source_root, destination_root)
 
 
-def copy_static(old_root, new_root):
-    rmtree(new_root)
-    mkdir(new_root)
-    crawl_directory("", ./static)
-
-def crawl_directory(current_path, target_directory)
-    ls = get_ls(join(current_path, target_directory))
+def copy_static(target_path, copy_path):
+    print(f"Moving to {target_path} and creating {copy_path}...")
+    mkdir(copy_path)
+    ls = get_ls(target_path)
     for item in ls:
-        pass
+        target_item_path = join(target_path, item)
+        copy_item_path = join(copy_path, item)
+        if isdir(target_item_path):
+            copy_static(target_item_path, copy_item_path)
+        elif isfile(target_item_path):
+            print(f"Copying {target_item_path} to {copy_item_path}...")
+            copy(target_item_path, copy_item_path)
+        else:
+            print("Item is neither directory nor file.")
 
 
 def get_ls(filepath):
     try:
         ls = listdir(filepath)
-        print(ls)
         return ls
     except Exception as e:
         print(e)
+        return []
 
 
 if __name__ == "__main__":
