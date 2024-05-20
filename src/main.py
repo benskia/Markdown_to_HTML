ba0abd2c72218bebaffcadab6eeaca2bdeea1bbe
benskia <benski.abraham@gmail.com>
2024-05-20 15:52:22 -0500

    update copy_static

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
-from os import path, listdir, mkdir
+from os import listdir, mkdir
+from os.path import exists, join, isfile
 from shutil import copy, rmtree
 from textnode import TextNode
 
 
 def main():
     source_directory = "./static"
-    desination_directory = "./public"
-    copy_static(source_directory, desination_directory)
+    destination_directory = "./public"
+    copy_static(source_directory, destination_directory)
 
 
-def copy_static(source, destination):
-    rmtree(destination)
-    mkdir(destination)
-    ls = get_ls(source)
+def copy_static(old_root, new_root):
+    rmtree(new_root)
+    mkdir(new_root)
+    crawl_directory("", ./static)
+
+def crawl_directory(current_path, target_directory)
+    ls = get_ls(join(current_path, target_directory))
     for item in ls:
         pass
 
